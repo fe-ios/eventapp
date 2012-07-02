21b82185746d0221b9c9ae0af873da16c39746f2
flashlizi <flashlizi@gmail.com>
2012-07-02 11:48:31 +0800

    Initial Commit

---
+++ EventApp/FEViewController.m
@@ -0,0 +1,34 @@
+//
+//  FEViewController.m
+//  EventApp
+//
+//  Created by zhenglin li on 12-7-2.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "FEViewController.h"
+
+@interface FEViewController ()
+
+@end
+
+@implementation FEViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
---
+++ EventApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EventApp
+//
+//  Created by zhenglin li on 12-7-2.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FEAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FEAppDelegate class]));
+    }
+}
