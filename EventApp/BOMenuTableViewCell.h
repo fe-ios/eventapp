44246f4f81aab8fe4d9dae1957c1af559065e71c
flashlizi <flashlizi@gmail.com>
2012-07-23 11:44:42 +0800

    new update

---
+++ EventApp/BOMenuTableViewCell.h
@@ -0,0 +1,19 @@
+//
+//  BOMenuTableViewCell.h
+//  EventApp
+//
+//  Created by Yin Zhengbo on 7/20/12.
+//  Copyright (c) 2012 snda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BOMenuTableViewCell : UITableViewCell
+
+@property (strong, nonatomic) UIImageView *backgroundImageView;
+@property (strong, nonatomic) UILabel *menuLabel;
+@property (strong, nonatomic) UIImageView *menuIcon;
+@property (strong, nonatomic) UILabel *detailLabel;
+
+
+@end
